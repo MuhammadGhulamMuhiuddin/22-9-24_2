aff68e797541fdddfa955d59042556d56c3460ec
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-09-27 09:30:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+ 
+// Question No.1
+
+var characters = ["a", "z", "l", "c", "d", "b","g", "h", "k" , "e", "i", "j", "m", "o", "q","p", "n", "k" ];
+
+var inOrder = characters.sort();
+
+console.log("Character's in Series:", inOrder);
+
+// Question No.2
+
+var characters = ["a", "z", "l", "c", "d", "b","g", "h", "k" , "e", "i", "j", "m", "o", "q","p", "n", "k" ];
+
+var disOrder = characters.reverse();
+
+console.log("Character's in UnSeries:", disOrder);
+
